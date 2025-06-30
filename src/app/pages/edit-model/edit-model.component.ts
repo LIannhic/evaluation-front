291c78e3ba3bbed9f16ab336824d649b5d24ae15
LIannhic <154102382+LIannhic@users.noreply.github.com>
2025-06-30 16:16:11 +0200

    correctif

--- src/app/pages/edit-model/edit-model.component.ts
+++ src/app/pages/edit-model/edit-model.component.ts
@@ -11,19 +11,20 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { NotificationService } from '../../services/notification.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-edit-produit',
+  selector: 'app-edit-model',
   imports: [FormsModule, ReactiveFormsModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule],
-  templateUrl: './edit-produit.component.html',
-  styleUrl: './edit-produit.component.scss',
+  templateUrl: './edit-model.component.html',
+  styleUrl: './edit-model.component.scss',
 })
-export class EditProduitComponent {
+export class EditModelComponent {
   formBuilder = inject(FormBuilder);
   http = inject(HttpClient);
   notification = inject(NotificationService);
   router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
 
 formulaire = this.formBuilder.group({
   pseudo: ['', [Validators.required, Validators.maxLength(20)]],
@@ -34,22 +35,37 @@ formulaire = this.formBuilder.group({
   date_naissance: [null, [Validators.required]],
 });
 
+modelEdite: any;
 
-  onAjoutModel() {
-    if (this.formulaire.valid) {
+ngOnInit() {
+  this.activatedRoute.params.subscribe((parametres) => {
+    if (parametres['id']) {
       this.http
-        .post('http://localhost:5000/model', this.formulaire.value)
-        .subscribe({
-          next: (reponse) => {
-            this.notification.show('Le model a bien été ajouté', 'valid');
-            this.router.navigateByUrl('/accueil');
-          },
-          error: (erreur) => {
-            if (erreur.status === 409) {
-              this.notification.show('Un model porte déjà ce pseudo', 'error');
-            }
-          },
+        .get('http://localhost:5000/model/' + parametres['id'])
+        .subscribe((model) => {
+          console.log(model);
+          this.formulaire.patchValue(model);
+          this.modelEdite = model;
         });
+    }
+  });
+}
+
+onAjoutModel() {
+  if (this.formulaire.valid) {
+    this.http
+      .post('http://localhost:5000/model', this.formulaire.value)
+      .subscribe({
+        next: (reponse) => {
+          this.notification.show('Le model a bien été ajouté', 'valid');
+          this.router.navigateByUrl('/accueil');
+        },
+        error: (erreur) => {
+          if (erreur.status === 409) {
+            this.notification.show('Un model porte déjà ce pseudo', 'error');
+          }
+        },
+      });
     }
   }
 }
